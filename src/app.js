f12fb0a8acb5264dd8534d9c7796a587fb3f3d55
ago <alek.golaszewski@gmail.com>
2018-01-18 21:01:17 +0100

    added changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,54 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+/* const numbers = [33,44,55,66,77];
+
+const numbers$ = Rx.Observable.from(numbers);
+
+numbers$.subscribe(
+    v => {
+        console.log(v);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('Completed')
+    });
+
+const posts = [
+    {title: 'Post One', body: 'This is the body'},
+    {title: 'Post Two', body: 'This is the body'},
+    {title: 'Post Three', body: 'This is the body'}
+];
+
+const postOutput = $('#posts');
+const posts$ = Rx.Observable.from(posts);
+
+posts$.subscribe(
+    post => {
+        console.log(post);
+        $('#posts').append('<li><h3>'+post.title+'</h3><p>'+post.body+'</p></li>')
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('Completed');
+    }
+) */
+
+const set = new Set(['Hello', 44, {title: 'My Title'}]);
+const set$ = Rx.Observable.from(set);
+
+set$.subscribe(
+    v => {
+        console.log(v);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('Completed');
+    }
+)
